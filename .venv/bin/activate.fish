902cd706fec81c46549c289979687ac82718a644
felipeleonfernandez <felipeleonfernandez@gmail.com>
2025-08-06 18:43:28 +0200

    Added basic functionality

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/felipe/workspace/development/python/projects/futmondo_assistant/.venv
+set -gx VIRTUAL_ENV /home/felipe/workspace/development/python/projects/fantasy-assistant/.venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
